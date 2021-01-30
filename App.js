702c50e65cd9adfcca628217ef6782c944f1b09d
Levi Zustiak <levizustiak@Pocket-Sand.local>
2021-01-29 23:09:38 -0600

    First commit of time tracker app

--- App.js
+++ App.js
@@ -1,21 +1,87 @@
+import  uuidv4 from 'uuid/v4';
+
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import EditableTimer from './components/EditableTimer';
+import ToggleableTimerForm from './components/ToggleableTimerForm';
+import { newTimer } from './utils/TimerUtils';
+
+export default class App extends React.Component {
+  state = {
+    timers: [
+      {
+        title: 'Mow the lawn',
+        project: 'House Chores',
+        id: uuidv4(),
+        elapsed: 5456099,
+        isRunning: true,
+      },
+      {
+        title: 'Bake squash',
+        project: 'Kitchen Chores',
+        id: uuidv4(),
+        elapsed: 1273998,
+        isRunning: false,
+      },
+    ],
+  };
+
+  handleCreateFormSubmit = timer => {
+    const { timers } = this.state;
+
+    this.setState({
+      timers: [newTimer(timer), ...timers],
+    });
+  };
+
+  render(){
+    const { timers } = this.state;
+
+    return (
+      <View style={styles.appContainer}>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>Timers</Text>
+        </View>
+        <ScrollView style={styles.timerList}>
+          <ToggleableTimerForm
+            onFormSubmit={this.handleCreateFormSubmit}
+          />
+          {timers.map(
+            ({ title, project, id, elapsed, isRunning }) => (
+              <EditableTimer
+                key={id}
+                id={id}
+                title={title}
+                project={project}
+                elapsed={elapsed}
+                isRunning={isRunning}
+              />
+            ),
+          )}
+        </ScrollView>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  titleContainer: {
+    paddingTop: 55,
+    paddingBottom: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: '#D6D7DA',
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  timerList: {
+    paddingBottom: 15,
   },
 });
---
+++ components/ToggleableTimerForm.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import TimerButton from './TimerButton';
+import TimerForm from './TimerForm';
+
+export default class ToggleableTimerForm extends React.Component {
+  state = {
+    isOpen: false,
+  };
+
+  handleFormOpen = () => {
+    this.setState({ isOpen: true });
+  };
+
+  handleFormClose = () => {
+    this.setState({ isOpen: false });
+  };
+
+  handleFormSubmit = timer => {
+
+    console.log("toggletimer submit");
+    const { onFormSubmit } = this.props;
+
+    onFormSubmit(timer);
+    this.setState({ isOpen: false });
+  };
+
+  render() {
+    const { isOpen } = this.state;
+
+    return (
+      <View
+        style={[styles.container, !isOpen && styles.buttonPadding]}
+      >
+        {isOpen ? (
+          <TimerForm
+            onFormSubmit={this.handleFormSubmit}
+            onFormClose={this.handleFormClose}
+          />
+        ) : (
+          <TimerButton
+            title="+"
+            color="black"
+            onPress={this.handleFormOpen}
+          />
+        )}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingVertical: 10,
+  },
+  buttonPadding: {
+    paddingHorizontal: 15,
+  },
+});
